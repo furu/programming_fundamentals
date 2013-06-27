6042a4c77126a70ddd0acace3a68a0317e911366
furu <ba09219@gmail.com>
2013-06-28 07:58:30 +0900

    問題8.1を解いた

---
+++ chapter-8/8.1.ml
@@ -0,0 +1,31 @@
+type book_t = {
+    title     : string;
+    author    : string;
+    publisher : string;
+    price     : int;
+    isbn      : string;
+}
+
+let book1 = {
+    title = "javascript_design_patterns";
+    author = "Addy Osmani";
+    publisher = "O'REILLY";
+    price = 2800;
+    isbn = "978-4-87311-618-1";
+}
+
+let book2 = {
+    title = "プログラミングの基礎";
+    author = "浅井健一";
+    publisher = "サイエンス社";
+    price = 2300;
+    isbn = "978-4-7819-1160-1";
+}
+
+let book3 = {
+    title = "プログラミング Haskell";
+    author = "Graham Hutton";
+    publisher = "オーム社";
+    price = 2800;
+    isbn = "978-4-274-06783-5";
+}
