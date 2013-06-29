cd87678c742a03ee05a35f7ca4db5a21dad9d51e
furu <ba09219@gmail.com>
2013-06-30 00:22:41 +0900

    問題8.2を解いた

---
+++ chapter-8/8.2.ml
@@ -0,0 +1,27 @@
+type okozukai_t = {
+    name : string;
+    price : int;
+    place : string;
+    date : string;
+}
+
+let okozukai1 = {
+    name = "モナ王";
+    price = 105;
+    place = "セブンイレブン";
+    date = "2013-08-29";
+}
+
+let okozukai2 = {
+    name = "ジャンプ";
+    price = 500;
+    place = "ヤマザキ";
+    date = "2013-04-01";
+}
+
+let okozukai3 = {
+    name = "プレミアムモルツ";
+    price = 300;
+    place = "イオン";
+    date = "2013-05-27";
+}
