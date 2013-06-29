51e16d248ea6a559c7549ea6fbe441fc196505bc
furu <ba09219@gmail.com>
2013-06-30 00:30:53 +0900

    問題8.3を解いた

---
+++ chapter-8/8.3.ml
@@ -0,0 +1,31 @@
+type person_t = {
+    name : string;
+    height : int;
+    weight : int;
+    birthday : string;
+    blood : string;
+}
+
+let person1 = {
+    name = "Bob";
+    height = 180;
+    weight = 60;
+    birthday = "5月1日";
+    blood = "A";
+}
+
+let person2 = {
+    name = "Alice";
+    height = 165;
+    weight = 45;
+    birthday = "11月29日";
+    blood = "O";
+}
+
+let person3 = {
+    name = "Kenny";
+    height = 200;
+    weight = 80;
+    birthday = "3月15日";
+    blood = "AB";
+}
